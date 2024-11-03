d837812c26370eef350d02bde4d20d66485af2dc
Kurt-Chan <kurtdeaustria@gmail.com>
2024-11-03 10:53:28 +0800

    added sound and audio player

--- src/pages/AnnivPage.tsx
+++ src/pages/AnnivPage.tsx
@@ -1,9 +1,19 @@
+import AudioPlayer from "@/components/Audio";
 import MouseImageTrail from "@/components/MouseImageTrail";
 // import { useEffect } from "react"
 // import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { HeartSpinner } from "react-spinners-kit";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
 
 
 
@@ -12,6 +22,8 @@ const AnnivPage = () => {
     const [imagesLoaded, setImagesLoaded] = useState(false);
     const date = new Date();
     const now = date.toLocaleDateString()
+
+    const bgSound = new Audio('https://res.cloudinary.com/dnhrwakmp/video/upload/v1730598110/anniv/j85itenlgfc2ducla0lj.mp3')
 
     const imageUrls = [
         'https://res.cloudinary.com/dnhrwakmp/image/upload/v1730513507/anniv/tytzjjmunwol1ezslfuv.png',
@@ -49,10 +61,18 @@ const AnnivPage = () => {
         );
     };
 
+    const handlePlaySound = () => {
+        bgSound.play()
+    }
+    const handlePauseSound = () => {
+        bgSound.pause()
+    }
+
     useEffect(() => {
         console.log(now)
+        bgSound.autoplay
 
-        if (now != "11/2/2024") { // must change to 11/18/2024 and change to date today
+        if (now != "11/18/2024") { // must change to 11/18/2024 and change to date today
             navigate('/wait')
         }
         else {
@@ -72,20 +92,40 @@ const AnnivPage = () => {
     }
 
     return (
-        <MouseImageTrail
-            renderImageBuffer={60}
-            rotationRange={25}
-            images={imageUrls}
-        >
-            <section className="grid h-screen w-full place-content-center bg-white">
-                <p className="flex flex-col items-center gap-2 text-3xl font-bold uppercase text-black">
-                    <span>Hover your mouse</span>
-                    <span className="text-sm font-normal text-center text-gray-600">
-                        USE YOUR LAPTOP OR DESKTOP FOR BETTER EXPERIENCE
-                    </span>
-                </p>
-            </section>
-        </MouseImageTrail>
+        <section className="grid grid-cols-12 divide-x-2 space-x-4">
+            <div className="col-span-8">
+                <MouseImageTrail
+                    renderImageBuffer={60}
+                    rotationRange={25}
+                    images={imageUrls}
+                >
+                    <section className="grid h-screen w-full place-content-center bg-white">
+                        <p className="flex flex-col items-center gap-2 text-3xl font-bold uppercase text-black">
+                            <span className="grid place-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                    <path fillRule="evenodd" d="M17.303 5.197A7.5 7.5 0 0 0 6.697 15.803a.75.75 0 0 1-1.061 1.061A9 9 0 1 1 21 10.5a.75.75 0 0 1-1.5 0c0-1.92-.732-3.839-2.197-5.303Zm-2.121 2.121a4.5 4.5 0 0 0-6.364 6.364.75.75 0 1 1-1.06 1.06A6 6 0 1 1 18 10.5a.75.75 0 0 1-1.5 0c0-1.153-.44-2.303-1.318-3.182Zm-3.634 1.314a.75.75 0 0 1 .82.311l5.228 7.917a.75.75 0 0 1-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 0 1-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 0 1-1.247-.606l.569-9.47a.75.75 0 0 1 .554-.68Z" clipRule="evenodd" />
+                                </svg>
+                                Hover your mouse</span>
+                            <span className="text-sm font-normal text-center text-gray-600">
+                                USE YOUR LAPTOP OR DESKTOP FOR BETTER EXPERIENCE
+                            </span>
+                        </p>
+                    </section>
+                </MouseImageTrail>
+            </div>
+            <div className="col-span-4 p-4 bg-rose-950">
+                <Card>
+                    <CardHeader className="text-center">
+                        <CardTitle>Fall</CardTitle>
+                        <CardDescription>Ben & Ben</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <AudioPlayer src='https://res.cloudinary.com/dnhrwakmp/video/upload/v1730598110/anniv/j85itenlgfc2ducla0lj.mp3'></AudioPlayer>
+                    </CardContent>
+                </Card>
+                <h1 className="text-lg">Love</h1>
+            </div>
+        </section>
     );
 }
 
