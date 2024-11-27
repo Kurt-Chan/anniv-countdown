3c11120d472f974e102e6212aa08d3b8af64fe47
Kurt-Chan <kurtdeaustria@gmail.com>
2024-11-27 15:27:06 +0800

    anniv commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css'
-import CountdownPage from './pages/CountdownPage';
+// import CountdownPage from './pages/CountdownPage';
 import AnnivPage from './pages/AnnivPage';
-import WhoAreYou from './pages/WhoAreYou';
+// import WhoAreYou from './pages/WhoAreYou';
 
 function App() {
 
   return (
     <>
       <Routes location={location} key={location.pathname}>
-        <Route index element={<WhoAreYou />} />
-        <Route path='/wait' element={<CountdownPage />} />
-        <Route path='/happy-anniversary' element={<AnnivPage />} />
+        <Route index element={<AnnivPage />} />
       </Routes>
     </>
   )
